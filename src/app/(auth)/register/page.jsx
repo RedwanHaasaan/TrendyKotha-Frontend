f978d2b240470d7094c8a5109013db35494aaedb
Redwan Hasan <redwanhasan.workspace@gmail.com>
2026-05-19 00:04:36 +0600

    Feat: Registraion Form implemented

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import Logo from '../../../../public/logo.png';
+import RegisterForm from '@/components/Form/RegisterForm';
+import Link from 'next/link';
+const Page = ()=>{
+    return(
+       <div className="min-h-screen flex items-center justify-center">
+            <div className="bg-white p-12 rounded-lg flex flex-col gap-8">
+                <div>
+                    <Image src={Logo} alt="Trendy Kotha Logo" width={100} height={100} className="mx-auto"/>
+                </div>
+                <div className='text-center flex flex-col gap-2'>
+                    <h1 className='text-4xl font-garamond text-primary font-medium'>Join the Circle</h1>
+                    <p className='text-base font-normal font-inter text-ancient'>Become a part of the digital manuscript.</p>
+                </div>
+                <RegisterForm/>
+                <div>
+                    <p className='text-sm font-inter text-ancient text-center'>Already in the circle?<Link href="/login" className='text-primary'> Enter here</Link></p>
+                </div>
+            </div>
+       </div>
+    )
+};
+
+export default Page;
---
+++ src/components/Form/RegisterForm.jsx
@@ -0,0 +1,74 @@
+"use client";
+import {
+  Button,
+  FieldError,
+  Form,
+  Input,
+  Label,
+  TextField,
+} from "@heroui/react";
+const RegisterForm = () => {
+  return (
+    <Form className="flex w-xs flex-col gap-4 font-inter">
+      <TextField
+        isRequired
+        name="fullName"
+        type="text"
+        validate={(value) => {
+          if (value.trim() === "") {
+            return "Full name is required";
+          }
+          return null;
+        }}
+      >
+        <Label className="uppercase">Full Name</Label>
+        <Input placeholder="Redwan Hasan"  className="rounded-sm custom-input"/>
+        <FieldError />
+      </TextField>
+      <TextField
+        isRequired
+        name="email"
+        type="email"
+        validate={(value) => {
+          if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+            return "Please enter a valid email address";
+          }
+          return null;
+        }}
+      >
+        <Label className="uppercase">Email</Label>
+        <Input placeholder="redwan@example.com"  className="rounded-sm custom-input"/>
+        <FieldError />
+      </TextField>
+      <TextField
+        isRequired
+        minLength={8}
+        name="password"
+        type="password"
+        validate={(value) => {
+          if (value.length < 8) {
+            return "Password must be at least 6 characters";
+          }
+          if (!/[A-Z]/.test(value)) {
+            return "Password must contain at least one uppercase letter";
+          }
+          if (!/[0-9]/.test(value)) {
+            return "Password must contain at least one number";
+          }
+          return null;
+        }}
+      >
+        <Label className="uppercase">Password</Label>
+        <Input placeholder="Enter your password" className="rounded-sm custom-input"/>
+        <FieldError />
+      </TextField>
+      <div className="flex gap-2">
+        <Button type="submit" className="w-full bg-primary text-white rounded-sm">
+          Register
+        </Button>
+      </div>
+    </Form>
+  );
+};
+
+export default RegisterForm;
