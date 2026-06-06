ae588fb6f5bccf2ecfb274d3b6d5025da8293320
Redwan Hasan <redwanhasan.workspace@gmail.com>
2026-06-06 07:20:43 +0600

    feat: improve ProtectedRoute with toast notifications for login and profile completion

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,39 +2,47 @@
 
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/hooks/useAuth";
-
+import toast from "react-hot-toast";
 
 export default function ProtectedRoute({ children }) {
   const router = useRouter();
-
-  const { loading, isAuthenticated ,user} = useAuth();
+  const hasShownToast = useRef(false);
+  const { loading, isAuthenticated, user } = useAuth();
 
   useEffect(() => {
     if (!loading && !isAuthenticated) {
-      router.replace("/login");
-    }
-    if (!user?.isProfileCompleted) {
-      router.replace(
-        "/dashboard/create-profile"
-      );
-      
-    }
-  }, [loading, isAuthenticated, user,router]);
+      if (!hasShownToast.current) {
+        toast.error("You must be logged in to access this page.");
 
-  if (loading) {
-    return (
-      <div className="flex items-center justify-center py-10">
-        Loading...
-      </div>
-    );
-  }
+        hasShownToast.current = true;
+      }
+
+      router.replace("/login");
+      return;
+    }
+    if (!loading && isAuthenticated && !user?.isProfileCompleted) {
+      if (!hasShownToast.current) {
+        toast.error("Please complete your profile to continue.");
+
+        hasShownToast.current = true;
+      }
+
+      router.replace("/dashboard/create-profile");
+      return;
+    }
+  }, [loading, isAuthenticated, user, router]);
 
   if (!isAuthenticated) {
     return null;
   }
 
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center py-10">Loading...</div>
+    );
+  }
   return children;
 }
