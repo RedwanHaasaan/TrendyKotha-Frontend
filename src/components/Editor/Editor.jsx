9548f17bbc50b9bb4d4d3a1bec4ccc971fb96a0b
Redwan Hasan <redwanhasan.workspace@gmail.com>
2026-06-16 18:24:36 +0600

    feat: update editor tools and dependencies for enhanced functionality; integrate new tools including CodeFlask, Nested Checklist, and Table

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect} from "react";
 import { EDITOR_TOOLS } from "./EditorTools";
-
+import TextVariantTune from "@editorjs/text-variant-tune";
 export default function Editor({editorRef}) {
 
 
@@ -16,6 +16,9 @@ export default function Editor({editorRef}) {
         holder: "editorjs",
         placeholder: "Let's write an awesome story!",
         tools: EDITOR_TOOLS,
+        tunes: {
+            textVariant: TextVariantTune,
+          },
       });
       await editor.isReady;
       editorRef.current = editor;
--- src/components/Editor/EditorTools.jsx
+++ src/components/Editor/EditorTools.jsx
@@ -1,16 +1,37 @@
 import Paragraph from "@editorjs/paragraph";
-import Quote from "@cychann/editorjs-quote";
-import TextStyle from "@skchawala/editorjs-text-style";
 import Header from "@editorjs/header";
+import Quote from "@cychann/editorjs-quote";
+
+import List from "@editorjs/list";
+import NestedList from "@editorjs/nested-list";
+
+import Checklist from "@editorjs/checklist";
+import NestedChecklist from "@calumk/editorjs-nested-checklist";
+
+import Table from "@editorjs/table";
+
+import CodeFlask from "@calumk/editorjs-codeflask";
+
+import Marker from "@editorjs/marker";
+import Underline from "@editorjs/underline";
+
+import ImageTool from "@editorjs/image";
+import LinkTool from "@editorjs/link";
+
 export const EDITOR_TOOLS = {
   paragraph: {
     class: Paragraph,
     inlineToolbar: true,
   },
+
   header: {
     class: Header,
     inlineToolbar: true,
     shortcut: "CMD+SHIFT+H",
+    config: {
+      levels: [1, 2, 3, 4, 5, 6],
+      defaultLevel: 2,
+    },
   },
 
   quote: {
@@ -18,11 +39,59 @@ export const EDITOR_TOOLS = {
     inlineToolbar: true,
   },
 
-  marker: {
-    class: TextStyle,
+  list: {
+    class: List,
     inlineToolbar: true,
+  },
+
+  nestedList: {
+    class: NestedList,
+    inlineToolbar: true,
+  },
+
+  checklist: {
+    class: Checklist,
+    inlineToolbar: true,
+  },
+
+  nestedChecklist: {
+    class: NestedChecklist,
+    inlineToolbar: true,
+  },
+
+  table: {
+    class: Table,
+    inlineToolbar: true,
+  },
+
+  codeFlask: {
+    class: CodeFlask,
+  },
+
+  marker: {
+    class: Marker,
+    shortcut: "CMD+SHIFT+M",
+  },
+
+  underline: {
+    class: Underline,
+    inlineToolbar: true,
+  },
+
+  linkTool: {
+    class: LinkTool,
     config: {
-      tag: "mark",
+      endpoint: "/api/editor/link",
+    },
+  },
+
+  image: {
+    class: ImageTool,
+    config: {
+      endpoints: {
+        byFile: "/api/editor/upload-image",
+        byUrl: "/api/editor/fetch-image",
+      },
     },
   },
 };
